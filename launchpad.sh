bd4121c957189648ea26c4276f1e086ba65cadda
AlbertoEnsiklope <makakitomakako69@gmail.com>
2024-08-09 00:50:59 +0200

    Update launchpad.sh

--- launchpad.sh
+++ launchpad.sh
@@ -12,8 +12,5 @@ sudo apt-get install -y docker-compose
 # Crear el directorio de configuración si no existe
 mkdir -p ./webtop_config
 
-# Navegar al directorio donde se encuentra el archivo docker-compose.yml
-cd /ruta/a/tu/docker-compose
-
 # Ejecutar Docker Compose
 sudo docker-compose up -d
