16fac8e028df00f5c431afba1834d3ee50bffa86
AlbertoEnsiklope <makakitomakako69@gmail.com>
2024-08-09 01:05:33 +0200

    Update launchpad.sh

--- launchpad.sh
+++ launchpad.sh
@@ -3,6 +3,12 @@
 # Actualizar el sistema
 sudo apt-get update
 
+# Intentar corregir problemas de paquetes retenidos
+sudo apt-get --fix-broken install -y
+
+# Instalar containerd
+sudo apt-get install -y containerd
+
 # Instalar Docker
 sudo apt-get install -y docker.io
 
@@ -12,5 +18,8 @@ sudo apt-get install -y docker-compose
 # Crear el directorio de configuración si no existe
 mkdir -p ./webtop_config
 
+# Navegar al directorio donde se encuentra el archivo docker-party.yml
+cd "$(dirname "$0")"
+
 # Ejecutar Docker Compose
-sudo docker-compose up -d
+sudo docker-compose -f docker-party.yml up -d
