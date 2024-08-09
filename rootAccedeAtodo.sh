02b671331de59c5a66b8309a4c259d74eb0b56aa
AlbertoEnsiklope <makakitomakako69@gmail.com>
2024-08-09 21:22:23 +0200

    Update rootAccedeAtodo.sh

--- rootAccedeAtodo.sh
+++ rootAccedeAtodo.sh
@@ -10,11 +10,11 @@ echo "alias la='ls -A'" >> /root/.bashrc
 echo "alias l='ls -CF'" >> /root/.bashrc
 
 # Copiar archivos de configuración del usuario normal a root
-cp /home/usuario/.bashrc /root/
-cp /home/usuario/.profile /root/
+cp /home/franco/.bashrc /root/
+cp /home/franco/.profile /root/
 
 # Cambiar permisos de directorios y archivos para que root tenga acceso completo
-chmod -R 755 /home/usuario
-chown -R root:root /home/usuario
+chmod -R 755 /home/franco
+chown -R root:root /home/franco
 
 echo "Configuración completada. Root ahora tiene acceso completo sin restricciones."
